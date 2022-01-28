e51de4ddeaebb9ed3cd88800fb150427075ebeeb
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2022-01-27 20:52:25 -0800

    server listening for graph microservice

---
+++ graph_ms/app.mjs
@@ -0,0 +1,24 @@
+'use strict'
+
+// import modules
+import express from 'express'
+import 'dotenv/config'
+import fetch from "node-fetch"
+
+// set intiial variables
+const PORT = 9000
+const app = express()
+
+// endpoints
+app.get('/', async (req, res) => {
+    res.send('hi')
+    // const path = 'http://localhost:3000/artist/skizzy mars'
+    // const response = await fetch(path, { method: 'GET' })
+    // const data = await response.json()
+    // res.send(data)
+})
+
+// listen on server
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`)
+})
