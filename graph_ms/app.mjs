f7dacca960fed2f09d0c213a3370d06a0136831f
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2022-01-29 21:46:45 -0800

    implement microservice for graphing

--- graph_ms/app.mjs
+++ graph_ms/app.mjs
@@ -3,51 +3,21 @@
 // import modules
 import express from 'express'
 import 'dotenv/config'
-import QuickChart from 'quickchart-js'
+import { chart_maker } from './utils/graphs.mjs'
+import { errorCatcher, errorHandler } from './utils/errorHelper.mjs'
 
 // set intiial variables
 const PORT = 9000
 const app = express()
 
+// middleware
+app.use(express.json())
+
 // endpoints
-app.get('/', async (req, res) => {
-    const chart = new QuickChart();
+app.post('/generateGraph', errorCatcher(chart_maker))
 
-    chart.setWidth(500)
-    chart.setHeight(300);
-
-    chart.setConfig({
-        type: 'radar',
-        data: {
-            labels: ['danceability', 'energy', 'speechiness', 'instrumentalness',
-                'acousticness', 'liveness', 'valence'],
-            datasets: [
-                {
-                    lineTension: .5,
-                    backgroundColor: 'rgba(178, 201, 235, 0.4)',
-                    borderColor: 'rgb(178, 201, 235)',
-                    pointBackgroundColor: 'rgba(94, 154, 238, 0.99)',
-                    data: [.53, .2, .43, .4, .5, .7],
-                },
-            ],
-        },
-        options: {
-            legend: { display: false },
-            title: {
-                display: true,
-                text: 'Musical Analysis',
-            },
-            scale: { ticks: { display: false } },
-        }
-    });
-    console.log(`<img src="${chart.getUrl()}/>"`)
-    res.send(`<img src="${chart.getUrl()}"/>`)
-
-    // const path = 'http://localhost:3000/artist/skizzy mars'
-    // const response = await fetch(path, { method: 'GET' })
-    // const data = await response.json()
-    // res.send(data)
-})
+// error handling
+app.use(errorHandler)
 
 // listen on server
 app.listen(PORT, () => {
---
+++ graph_ms/utils/graphs.mjs
@@ -0,0 +1,40 @@
+'use strict'
+
+import QuickChart from 'quickchart-js'
+
+// mandatory
+//    config - values going into setConfig
+
+// optional
+//    width - int
+//    height - int
+//    format - png | svg
+//    backgroundColor - any valid HTML color works
+//    useShort - True/False (uses webservice to create fixed-length URL)
+
+const chart_maker = (async (req, res) => {
+    const { config, width, height, format, backgroundColor, useShort } = req.body
+    const chart = new QuickChart();
+
+    // main chart settings
+    if (typeof config === 'undefined') { throw new Error('Config not sent!') }
+    chart.setConfig(config)
+
+    // optional chart settings
+    if (typeof width !== 'undefined') { chart.setWidth(width) }
+    if (typeof height !== 'undefined') { chart.setHeight(height) }
+    if (typeof format !== 'undefined') { chart.setFormat(format) }
+    if (typeof backgroundColor !== 'undefined') { chart.setBackgroundColor(backgroundColor) }
+
+    // generate url
+    let url
+    if (typeof useShort !== 'undefined' && useShort === true) {
+        url = await chart.getShortUrl()
+    } else {
+        url = chart.getUrl()
+    }
+
+    res.send(url)
+})
+
+export { chart_maker }
