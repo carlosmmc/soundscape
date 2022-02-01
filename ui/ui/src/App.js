34c7c62549f326f7b9bfa8d34e69a0ab570bea0e
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2022-01-31 21:47:21 -0800

    implement basic form and add icon

--- ui/ui/src/App.js
+++ ui/ui/src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          first edit!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+
+          <fieldset>
+            <legend>soundscape artist search</legend>
+            <label>artist name
+              <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+            </label>
+          </fieldset>
+          <button
+            onClick={e => {
+              setSearchTerm(e.target.value)
+              alert(`you searched for "${searchTerm}"`)
+              e.preventDefault()
+            }}>
+            Submit </button>
+        </form>
       </header>
     </div>
   );
