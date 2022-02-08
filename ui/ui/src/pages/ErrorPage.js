10a2ae0fe29bac39bceccafe54de5132c4422fdb
Carlos Martinez-Cabello <carlosmmc123@gmail.com>
2022-02-07 21:41:07 -0800

    styled error page

--- ui/ui/src/pages/ErrorPage.js
+++ ui/ui/src/pages/ErrorPage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import '../styling/error.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Looks like something went wrong. Go back to the home page and try again!</p>
-            <Link to="/">Home Page</Link>
+        <div className="ErrorPage">
+            <h1 className="ErrorTitle">Whoops!</h1>
+            <h4 className="SubTitle">Page Not Found</h4>
+            <p className="ErrorText">Something went wrong & we couldn't find the page you requested! Try going back to the home page and trying that again.</p>
+            <Link to="/" className='GoHome'>Home Page</Link>
         </div>
     );
 }
